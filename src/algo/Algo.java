bb7c2d1068008596accaf2664cc1bbd91bca5912
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2018-02-24 15:15:10 +0700

    update soal

--- src/algo/Algo.java
+++ src/algo/Algo.java
@@ -5,6 +5,8 @@
  */
 package algo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author yusuf
@@ -16,7 +18,7 @@ public class Algo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println(soal2(9));
+        soal3(3);
     }
 
     public static void soal1(int input) {
@@ -32,12 +34,12 @@ public class Algo {
                         } else {
                             System.out.print("  ");
                         }
-                    }else{
-                       if (i == 0 ) {
+                    } else {
+                        if (i == 0) {
                             System.out.print("* ");
                         } else {
                             System.out.print("  ");
-                        } 
+                        }
                     }
                     x++;
                 }
@@ -46,18 +48,51 @@ public class Algo {
         }
     }
 
-    public static boolean soal2(int input){
+    public static boolean soal2(int input) {
         if (input < 2) {
             return false;
-        }else if(input == 2){
+        } else if (input == 2) {
             return true;
-        }else{
-            for (int i = 2; i < input-1; i++) {
+        } else {
+            for (int i = 2; i < input - 1; i++) {
                 if (input % i == 0) {
                     return false;
                 }
             }
         }
         return true;
+    }
+
+    public static void soal3(int dadu) {
+        dadu(dadu, new ArrayList<Integer>(), 1);
+    }
+
+    public static void dadu(int jumlahDadu, ArrayList<Integer> data, int posisi) {
+        if (posisi == jumlahDadu) {
+            for (int i = 1; i <= 6; i++) {
+                data.add(i);
+                print(data);
+                data.remove(data.size()-1);
+            }
+        } else {
+            for (int i = 1; i <= 6; i++) {
+                data.add(i);
+                dadu(jumlahDadu, data, posisi+1);
+                data.remove(data.size()-1);
+            }
+        }
+    }
+
+    public static void print(ArrayList<Integer> data) {
+        System.out.print("[");
+        for (int i = 0; i < data.size(); i++) {
+            if (i == data.size() - 1) {
+                System.out.print(data.get(i) + "");
+            } else {
+                System.out.print(data.get(i) + ",");
+            }
+        }
+        System.out.print("]");
+        System.out.println("");
     }
 }
