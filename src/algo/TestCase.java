5b9d2d1bfe48371bd6f2abdbeaf54faa83210969
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2018-03-01 22:31:25 +0700

    update test case

--- src/algo/TestCase.java
+++ src/algo/TestCase.java
@@ -11,4 +11,34 @@ package algo;
  */
 public class TestCase {
     
+    public static int soal1_1 = 4; 
+    
+    public static int soal1_2 = 7; 
+    
+    public static int soal2_1a = 2; 
+    public static int soal2_1b = 7; 
+    
+    public static int soal2_2a = 4; 
+    public static int soal2_2b = 11; 
+    
+    public static int soal3_1 = 3;
+    
+    public static int soal3_2 = 5;
+    
+    public static int soal4_1 = 12345;
+    
+    public static int soal4_2 = 91298;
+    
+    public static int soal5_1[][] = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+    
+    public static int soal5_2[][] = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 16}
+        };
 }
