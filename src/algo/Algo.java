dce0f2c037ff228cf69482690f7e8b75416b02f2
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2018-03-01 22:31:38 +0700

    update soal

--- src/algo/Algo.java
+++ src/algo/Algo.java
@@ -13,37 +13,31 @@ import java.util.TreeMap;
  * @author yusuf
  */
 public class Algo {
-    
-   
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
-//        recursifSum(100, 2, new ArrayList<Integer>());
-        int a[][] = {
-            {1, 2, 3, 4},
-            {5, 6, 7, 8},
-            {9, 10, 11, 12},
-            {13, 14, 15, 16}
-        };
-        
         int asas[] = {0, 1, 0, 3, 12};
-        
-        int as[][] =   {{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
-                        {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
-                        {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}};
-//        soal1(4); //easy
-//        soal2(7,11); //easy
-//        soal3(4); //hard
-//        soal4(12345); //medium
-//        soal5(a); //medium
+
+        int as[][] = {{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+        {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+        {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}};
+//        soal1(TestCase.soal1_1); //easy
+//        soal1(TestCase.soal1_2); //easy
+//        soal2(TestCase.soal2_1a,TestCase.soal2_1b); //easy
+//        soal2(TestCase.soal2_2a,TestCase.soal2_2b); //easy
+//        soal3(TestCase.soal3_1); //hard
+//        soal3(TestCase.soal3_2); //hard
+//        soal4(TestCase.soal4_1); //medium
+//        soal4(TestCase.soal4_2); //medium
+//        soal5(TestCase.soal5_1); //medium
+//        soal5(TestCase.soal5_2); //medium
 //        soal6("qqweqwqs"); //easy
 //        soal7(1241, 3); //hard
 //        soal8(a); //hard
@@ -112,50 +106,50 @@ public class Algo {
     public static void soal6(String kalimat) {
         Easy.jumlahKarakter(kalimat);
     }
-    
+
     public static void soal7(int x, int n) {
         Hard.powerSum(x, n);
     }
-    
+
     public static void soal8(int x[][]) {
         Hard.spiral(x);
     }
-    
+
     public static void soal9(int n) {
         printArray(Hard.generateMatrixSpiral(n));
     }
-    
+
     public static void soal10(String kalimat) {
         printString(Medium.listPalindrom(kalimat));
     }
-    
+
     public static void soal11(int[] nums) {
         int hasil[] = Easy.moveZeroes(nums);
         for (int i = 0; i < hasil.length; i++) {
             System.out.print(hasil[i] + " ");
         }
     }
-    
+
     public static void soal12(int ke) {
         print(Medium.pascal(ke));
     }
-    
+
     public static void soal13(int nums[]) {
         ArrayList<Integer> a = Easy.countSmaller(nums);
         for (int i = 0; i < a.size(); i++) {
-            System.out.print(a.get(i)+" ");
+            System.out.print(a.get(i) + " ");
         }
     }
-    
-    public static void soal14(int[][] a){
+
+    public static void soal14(int[][] a) {
         int total = Hard.maxAreaOfIsland(a);
         System.out.println(total);
     }
-    
-    public static void soal15(int num){
+
+    public static void soal15(int num) {
         System.out.println(Medium.intToRoman(num));
     }
-    
+
     public static void print(ArrayList<Integer> data) {
         System.out.print("[");
         for (int i = 0; i < data.size(); i++) {
@@ -168,7 +162,7 @@ public class Algo {
         System.out.print("]");
         System.out.println("");
     }
-    
+
     public static void printArray(int[][] args) {
         for (int i = 0; i < args.length; i++) {
             for (int j = 0; j < args[i].length; j++) {
@@ -177,18 +171,18 @@ public class Algo {
             System.out.println("");
         }
     }
-    
+
     static void printManeh(ArrayList<Integer> hasil) {
         for (int i = 0; i < hasil.size(); i++) {
             System.out.print(hasil.get(i) + " ");
         }
         System.out.println("");
     }
-    
+
     static void printString(ArrayList<String> hasil) {
         for (int i = 0; i < hasil.size(); i++) {
             System.out.println(hasil.get(i));
         }
     }
-    
+
 }
